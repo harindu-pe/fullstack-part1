4fde70307b74d2d4dae3e396970756a8c61daef9
harindu-pe <harindu.pe@gmail.com>
2025-01-16 23:46:32 +0530

    exercise 1.10 done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+);
+
 // a proper place to define a component
 const Statistics = ({ good, neutral, bad }) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return <div>No feedback given</div>;
   }
 
-  console.log(good, neutral, bad);
-
   return (
     <div>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>
-        average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
-      </div>
-      <div>positive {good / (good + neutral + bad)}</div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine
+        text="average"
+        value={(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      />
+      <StatisticLine text="positive" value={good / (good + neutral + bad)} />
     </div>
   );
 };
@@ -36,9 +43,9 @@ const App = () => {
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={handleGood}>good</button>
-        <button onClick={handleNeutral}>neutral</button>
-        <button onClick={handleBad}>bad</button>
+        <Button onClick={handleGood} text="good" />
+        <Button onClick={handleNeutral} text="neutral" />
+        <Button onClick={handleBad} text="bad" />
       </div>
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
